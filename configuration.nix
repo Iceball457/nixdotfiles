7a837fcd8345e50b1bfb00c639acf3f648d7ad91
Frost Flores <iceball457@gmail.com>
2025-05-03 06:32:27 -0700

    fix: syntax error

--- configuration.nix
+++ configuration.nix
@@ -85,11 +85,11 @@
 		nixd
 	];
 
-	pkgs.mkShell {
+	pkgs.mkShell = {
 		buildInputs = with pkgs; [
 			rnix-lsp
 		];
-	}
+	};
 
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
