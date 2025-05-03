de0c1a9f2ddd10d22260084490e74d597d187e82
Frost Flores <iceball457@gmail.com>
2025-05-03 10:13:26 -0700

    fix: restored messed up nix configuration

--- flake.nix
+++ flake.nix
@@ -15,6 +15,14 @@
   outputs = inputs@{ self, nixpkgs, ... }: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
+    nixosConfigurations.coldsnap = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs; };
+      modules = [
+        ./configuration.nix
+        ./hypr.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
     homeConfigurations = {
     "frosti@coldsnap" = inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
@@ -25,7 +33,7 @@
             home.homeDirectory = "/home/frosti";
           }
           (
-            { pkgs, ...}:
+            { ... }:
             {
               wayland.windowManager.hyprland.enable = true;
             }
