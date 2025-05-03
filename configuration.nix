747c9a3274750d2b92ee20200fb596e01ac4043f
Frost Flores <iceball457@gmail.com>
2025-05-03 06:11:27 -0700

    feat: enable trash

--- configuration.nix
+++ configuration.nix
@@ -84,6 +84,9 @@
 		wev
 	];
 
+	# Enable trash
+	services.gvfs.enable = true;
+
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
 	# programs.mtr.enable = true;
