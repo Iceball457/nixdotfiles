5ec15ae1d5533f093077de3617ec1d16577d9e60
Frost Flores <iceball457@gmail.com>
2025-05-03 10:21:04 -0700

    fix: change home manager package requests to use home.packages

--- home.nix
+++ home.nix
@@ -2,13 +2,14 @@
 {
   programs.steam.enable = true;
   hardware.opentabletdriver.enable = true;
-
-  environment.systemPackages = with pkgs; [
+  
+  home.packages = with pkgs; [
     gimp
 		obs-studio
 		dissent # Discord
 		openrgb-with-all-plugins
   ];
+  
   home.file.".config/".recursive = ./frosti;
   xdg.configFile = {
     "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
