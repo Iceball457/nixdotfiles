6fb3694c0221c04e5f6b251c2731e6a59c805b82
Frost Flores <iceball457@gmail.com>
2025-05-03 17:33:47 -0700

    Revert "fix: revert to pulse audio"
    This reverts commit 5099fec343e23773fa8b80192f53c87426ea7e2b.

--- hypr.nix
+++ hypr.nix
@@ -2,15 +2,15 @@
 {
 	services.displayManager.sddm.wayland.enable = true;
 	services.displayManager.sddm.enable = true;
-	services.pulseaudio.enable = true;
-	# security.rtkit.enable = true;
-	# services.pipewire = {
-	# 	enable = true;
-	# 	alsa.enable = true;
-	# 	alsa.support32Bit = true;
-	# 	pulse.enable = true;
-	# 	jack.enable = true;
-	# };
+	services.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		jack.enable = true;
+	};
 	
 
 	programs.hyprland.enable = true;
