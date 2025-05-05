d08d9514997797ba36cff152e349ae36d9b5efcf
Frost Flores <iceball457@gmail.com>
2025-05-05 06:30:05 -0700

    fix: Add udev rules for my tablet

--- hypr.nix
+++ hypr.nix
@@ -8,6 +8,13 @@
 
 	hardware.opentabletdriver.enable = true;
 
+	services.udev.extraRules = ''
+# XP-Pen Artist 24
+KERNEL=="hidraw*", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", TAG+="uaccess", TAG+="udev-acl"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", TAG+="uaccess", TAG+="udev-acl"
+SUBSYSTEM=="input", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+	'';
+
 	environment.systemPackages = with pkgs; [
 		kitty
 		waybar
