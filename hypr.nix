2a5d8fcaa82695bf33477b162b49ee7908b28870
Frost Flores <iceball457@gmail.com>
2025-09-17 20:54:16 -0700

    Remove Open Tablet Drivers

--- hypr.nix
+++ hypr.nix
@@ -16,14 +16,12 @@ in {
 
 	programs.steam.enable = true;
 
-	hardware.opentabletdriver.enable = true;
-
-	services.udev.extraRules = ''
-# XP-Pen Artist 22r
-KERNEL=="hidraw*", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", TAG+="uaccess", TAG+="udev-acl"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", TAG+="uaccess", TAG+="udev-acl"
-SUBSYSTEM=="input", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-'';
+# 	services.udev.extraRules = ''
+# # XP-Pen Artist 22r
+# KERNEL=="hidraw*", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", TAG+="uaccess", TAG+="udev-acl"
+# SUBSYSTEM=="usb", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", TAG+="uaccess", TAG+="udev-acl"
+# SUBSYSTEM=="input", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="091b", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+# '';
 
 	environment.systemPackages = with pkgs; [
 		kitty
